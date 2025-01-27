2995219a3bfca7a1e1f5b18931e2b9a7294a733d
Monisha R <rajumonisha024@gmail.com>
2025-01-27 08:46:10 -0500

    Trees 1 - Completed Problem 1
    Validate Binary Search

---
+++ validateBST.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        self.prev = None
+        self.isValid = True
+        
+        if root is None:
+            return true
+        self.inorder(root)
+        return self.isValid
+
+    def inorder(self, root: Optional[TreeNode]):
+        if root is None:
+            return
+        self.inorder(root.left)
+        if self.prev is not None and self.prev.val >= root.val:
+            self.isValid = False
+        self.prev = root
+        self.inorder(root.right)
